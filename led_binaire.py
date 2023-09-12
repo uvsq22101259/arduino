785cd800be9b24ad814a9b9266404bb708b2ab1e
uvsq22101259 <elie.kanga@ens.uvsq.fr>
2023-09-12 10:35:36 +0200

    compteurs bin

--- led_binaire.py
+++ led_binaire.py
@@ -8,16 +8,19 @@ vert = carte.get_pin("d:3:o")
 jaune = carte.get_pin("d:4:o")
 sleep(1.0)
 led = [jaune, vert, bleu ] 
-
-
-print("debut du test")
-for elem,binaire in zip(led,input("entre du binaire ")):
-    if int(binaire):
-        elem.write(1)
-sleep(2.0)
 out = lambda t : t.write(0) 
-# for elem in led:
-#     out(elem)
-list(map(out,led))
+print("debut du test")
+for i in range(8):
+    mot = bin(i)[2:]
+    while len(mot) != 3:
+        mot = "0"+mot
+    for elem,binaire in zip(led,mot):
+        if int(binaire):
+            elem.write(1)
+    sleep(1.0)
+    list(map(out,led))
+
+
+
 print("fin du test")
 carte.exit()
